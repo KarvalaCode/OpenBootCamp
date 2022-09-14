7724f87fa3e5fcd12ed36df48cf9500d8015c1f0
Karvala <77838356+KarvalaCode@users.noreply.github.com>
2022-09-14 19:21:20 +0200

    Add files via upload

---
+++ JavaScript/Ejercicio2/index.js
@@ -0,0 +1,30 @@
+/* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+const libroFav = {
+    titulo: "A long way Down",
+    autor: "Nick Hornby",
+    publicacion: 2005,
+    url: "https://www.todostuslibros.com/libros/en-picado_978-84-339-7363-4"
+};
+    
+birthDate = new Date(1982,09, 03, 01, 30);
+
+const lista = [
+    nombre ="Sara",
+    edad = 39,
+    isDev = false,
+    date = birthDate.toString(),
+    libroFavorito = libroFav
+];
+
+console.log (lista);
