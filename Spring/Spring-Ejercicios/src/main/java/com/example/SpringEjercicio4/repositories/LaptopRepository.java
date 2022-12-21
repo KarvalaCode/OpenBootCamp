07d2332510cded9059e2b2813b161d85bd0293f6
Karvala <77838356+KarvalaCode@users.noreply.github.com>
2022-12-21 12:33:17 +0100

    Delete Spring/Spring-Ejercicios directory

--- Spring/Spring-Ejercicios/src/main/java/com/example/SpringEjercicio4/repositories/LaptopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SpringEjercicio4.repositories;
-
-import com.example.SpringEjercicio4.entities.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LaptopRepository extends JpaRepository<Laptop, Long> {
-}
